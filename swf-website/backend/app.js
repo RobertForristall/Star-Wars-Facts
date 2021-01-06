9c9e8dc2ef1d8b60e7fe77ba312f6e9dd5bc551d
RobertForristall <59899671+RobertForristall@users.noreply.github.com>
2021-01-06 18:04:09 -0500

    Created main server app, env file, and folders

---
+++ swf-website/backend/app.js
@@ -0,0 +1,30 @@
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+require('dotenv').config()
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+const port = process.env.PORT || 5000
+
+const uri = process.env.ATLAS_URI
+
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+
+const connection = mongoose.connection
+
+connection.once('open', () => {
+    console.log('Connection to MongoDB Atlas Succesful')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
+
+module.exports = app
