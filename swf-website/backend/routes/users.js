02003a77b421972bf330fd128bece51e3159713d
RobertForristall <59899671+RobertForristall@users.noreply.github.com>
2021-01-07 12:16:06 -0500

    installed auth/mail packages, updated users router

---
+++ swf-website/backend/routes/users.js
@@ -0,0 +1,57 @@
+const router = require('express').Router()
+
+const jwt = require('jsonwebtoken')
+
+const crypto = require('crypto')
+
+const nodemailer = require('nodemailer')
+
+let User = require('../models/user.model')
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+
+    auth: {
+        user: 'forristallcontacts@gmail.com',
+        pass: 'tbxy4218'
+    }
+
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.heders['authorization']
+
+    if (authHeader.split(' ')[0] !== 'Bearer') return res.status(403).json('Token Authorization Failed, error code: TA1')
+
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null) return res.status(401).json('Missing Token')
+
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        console.log(err)
+
+        if (err) return res.status(403).json('Token Authorization Failed, error code: TA2')
+
+        req.user = user
+        next()
+    })
+}
+
+function sendVerificationEmail(user) {
+    const mailOptions = {
+        from: 'forristallcontacts@gmail.com',
+
+        to: user.email,
+
+        subject: 'Email Verification - Star Wars Facts',
+
+        html: '<p>Click <a href="http://localhost:5000/users/verify/' + user.verificationKey + '">here</a> to verify your email</p>'
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) console.log(err)
+
+        else console.log('Email sent: ' + info.response)
+    })
+}
+
