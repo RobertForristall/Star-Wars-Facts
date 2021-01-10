0fa77756329c03db17b180e048aced90e2b93b88
RobertForristall <59899671+RobertForristall@users.noreply.github.com>
2021-01-10 12:09:18 -0500

    Continued work on sidenav

--- swf-website/src/components/Navbar.component.js
+++ swf-website/src/components/Navbar.component.js
@@ -12,13 +12,42 @@ export default class Navbar extends React.Component {
 
     render() {
         return(
-            <div classname = 'sidenav'>
-                <div classname = 'MainImage'>
+            <div className = 'sidenav'>
+                <div className = 'MainImage'>
                     <img src = {image} alt = "Death Star" width = '200' height = '200'/>
                 </div>
-                <Link to = '/'>Main Page</Link>
-                <Link to = '#'>Films</Link>
-                <Link to = '#'>Characters</Link>
+                <h3 className = 'WhiteColor'>Main Links:</h3>
+                <ul>
+                    <li>
+                        <Link to = '/'>Home Page</Link>
+                    </li>
+                    <li>
+                        <Link to = '#'>Forms</Link>
+                    </li>
+                    <li>
+                        <Link to = '#'>Recent Changes</Link>
+                    </li>
+                    <li>
+                        <Link to = '#'>News</Link>
+                    </li>
+                    <li>
+                        <Link to ='#'>Random Article</Link>
+                    </li>
+                    <li>
+                        <Link to = '#'>About</Link>
+                    </li>
+                </ul>
+
+                <button className='dropdown-btn'>Films
+                    <i className = 'fa fa-caret-down'/>
+                </button>
+                <div className = 'dropdown-container'>
+                    <Link to = '#'>The Phantom Menace</Link>
+                    <Link to = '#'>Attack of the Clones</Link>
+                    <Link to = '#'>Revenge of the Sith</Link>
+                </div>
+
+                
             </div>
         )
     }
